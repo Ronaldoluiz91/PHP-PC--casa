9a2eddf0418887ebb003e9a25d08aed4768dfc9b
Ronaldo <159172761+Ronaldoluiz91@users.noreply.github.com>
2024-07-05 14:15:54 +0000

    atualizando

--- exercicios2.php
+++ exercicios2.php
@@ -23,26 +23,3 @@ if ($num < 2) {
 
 
 
-
-// Função para verificar se um número é primo
-// function primo($num) {
-//     // Se o número for menor que 2, não é primo
-//     if ($num < 2) {
-//         return false;
-//     }
-//     // Verifica se o número tem divisores além de 1 e ele mesmo
-//     for ($i = 2; $i <= sqrt($num); $i++) {
-//         if ($num % $i == 0) {
-//             return false;
-//         }
-//     }
-
-//     return true;
-// }
-
-// // Verifica se o número é primo e imprime o resultado
-// if (primo($num)) {
-//     echo "É um numero primo <hr>";
-// } else {
-//     echo "Não é um numero primo <hr>";
-// }
